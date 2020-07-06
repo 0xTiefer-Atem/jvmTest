c3f8869c33f852a002cf406bc3dcd64ad337bff1
0xMilk <1144502582@qq.com>
2020-07-06 11:39:35 +0800

    获取类加载其的方式

---
+++ src/main/java/org/wq/jvm/chapter02/ClassLoaderTest2.java
@@ -0,0 +1,21 @@
+package org.wq.jvm.chapter02;
+
+public class ClassLoaderTest2 {
+    public static void main(String[] args) {
+        try {
+            // 1.
+            ClassLoader classLoader = Class.forName("java.lang.String").getClassLoader();
+            System.out.println(classLoader);
+
+            // 2.
+            ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
+            System.out.println(contextClassLoader);
+            
+            // 3.
+            ClassLoader systemClassLoader = ClassLoader.getSystemClassLoader();
+            System.out.println(systemClassLoader);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
