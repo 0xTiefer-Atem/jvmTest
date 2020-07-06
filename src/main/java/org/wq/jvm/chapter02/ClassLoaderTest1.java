6601c6a7ea61baeaf01b36246dfc405f301cc1f8
0xMilk <1144502582@qq.com>
2020-07-06 11:00:03 +0800

    从上面路径随意选择一个类，验证它的类加载器

--- src/main/java/org/wq/jvm/chapter02/ClassLoaderTest1.java
+++ src/main/java/org/wq/jvm/chapter02/ClassLoaderTest1.java
@@ -1,6 +1,7 @@
 package org.wq.jvm.chapter02;
 
 import java.net.URL;
+import java.security.Provider;
 
 public class ClassLoaderTest1 {
     public static void main(String[] args) {
@@ -11,5 +12,9 @@ public class ClassLoaderTest1 {
         for (URL u:urls) {
             System.out.println(u.toExternalForm());
         }
+
+        //从上面路径随意选择一个类，验证它的类加载器是谁:  BootStrapClassLoader
+        ClassLoader classLoader = Provider.class.getClassLoader();
+        System.out.println(classLoader);
     }
 }
