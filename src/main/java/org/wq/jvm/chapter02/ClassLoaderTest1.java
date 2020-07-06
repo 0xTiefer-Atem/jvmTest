15a84e4d7c500db0bc63bc4522ff207f6a88bc12
0xMilk <1144502582@qq.com>
2020-07-06 10:56:47 +0800

    获取BootstrapClassLoader能够加载api的路径

--- src/main/java/org/wq/jvm/chapter02/ClassLoaderTest1.java
+++ src/main/java/org/wq/jvm/chapter02/ClassLoaderTest1.java
@@ -1,7 +1,15 @@
 package org.wq.jvm.chapter02;
 
+import java.net.URL;
+
 public class ClassLoaderTest1 {
     public static void main(String[] args) {
-        
+        System.out.println("启动类加载器");
+
+        //获取BootstrapClassLoader能够加载api的路径
+        URL[] urls = sun.misc.Launcher.getBootstrapClassPath().getURLs();
+        for (URL u:urls) {
+            System.out.println(u.toExternalForm());
+        }
     }
 }
