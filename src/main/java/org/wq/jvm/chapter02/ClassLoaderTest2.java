ff9f1b063279950339abd7209710874fb567be45
0xMilk <1144502582@qq.com>
2020-07-06 11:39:55 +0800

    修改

--- src/main/java/org/wq/jvm/chapter02/ClassLoaderTest2.java
+++ src/main/java/org/wq/jvm/chapter02/ClassLoaderTest2.java
@@ -10,7 +10,7 @@ public class ClassLoaderTest2 {
             // 2.
             ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
             System.out.println(contextClassLoader);
-            
+
             // 3.
             ClassLoader systemClassLoader = ClassLoader.getSystemClassLoader();
             System.out.println(systemClassLoader);
