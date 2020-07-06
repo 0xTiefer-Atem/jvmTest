7608d177aa066f2f9c6c7fd5a84a61297fbae42a
0xMilk <1144502582@qq.com>
2020-07-06 11:05:09 +0800

    扩展类加载器验证

--- src/main/java/org/wq/jvm/chapter02/ClassLoaderTest1.java
+++ src/main/java/org/wq/jvm/chapter02/ClassLoaderTest1.java
@@ -1,5 +1,7 @@
 package org.wq.jvm.chapter02;
 
+import sun.security.ec.CurveDB;
+
 import java.net.URL;
 import java.security.Provider;
 
@@ -16,5 +18,16 @@ public class ClassLoaderTest1 {
         //从上面路径随意选择一个类，验证它的类加载器是谁:  BootStrapClassLoader
         ClassLoader classLoader = Provider.class.getClassLoader();
         System.out.println(classLoader);
+
+
+        System.out.println("扩展类加载器");
+        String extDir = System.getProperty("java.ext.dirs");
+        for (String path: extDir.split(":")) {
+            System.out.println(path);
+        }
+
+        //验证
+        ClassLoader classLoader1 = CurveDB.class.getClassLoader();
+        System.out.println(classLoader1);
     }
 }
