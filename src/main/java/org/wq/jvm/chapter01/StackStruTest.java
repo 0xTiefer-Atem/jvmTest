f448e82ee08e4102a98a779f74054f072825ad83
0xMilk <1144502582@qq.com>
2020-07-06 10:49:36 +0800

    java编译器基于斩的指令集架构的例子

---
+++ src/main/java/org/wq/jvm/chapter01/StackStruTest.java
@@ -0,0 +1,12 @@
+package org.wq.jvm.chapter01;
+
+public class StackStruTest {
+    public static void main(String[] args) {
+        int i = 1 + 2;
+        int j = 2;
+        int k = 3;
+        int m = j + k;
+        int w = Integer.parseInt("01");
+        System.out.println(w);
+    }
+}
