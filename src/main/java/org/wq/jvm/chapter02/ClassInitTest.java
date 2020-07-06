39ec69a09a896056fed61be45d7bcc0cf765a852
0xMilk <1144502582@qq.com>
2020-07-06 10:50:16 +0800

    <clinit>()函数生成条件

---
+++ src/main/java/org/wq/jvm/chapter02/ClassInitTest.java
@@ -0,0 +1,20 @@
+package org.wq.jvm.chapter02;
+
+
+//有static修饰时，jvm才会生成<clinit>()方法，没有就不会生成
+public class ClassInitTest {
+    private static int num = 1;
+    static {
+        num = 2;
+        number = 20;
+        System.out.println(num);
+//        System.out.println(number);//报错：非法的前向引用
+    }
+
+    private static int number = 10;
+
+    public static void main(String[] args) {
+        System.out.println(ClassInitTest.num);
+        System.out.println(ClassInitTest.number);
+    }
+}
