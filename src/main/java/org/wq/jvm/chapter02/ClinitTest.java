94021e21608552a9df68c78ba942422b8e22b689
0xMilk <1144502582@qq.com>
2020-07-07 22:02:01 +0800

    验证cinit函数

---
+++ src/main/java/org/wq/jvm/chapter02/ClinitTest.java
@@ -0,0 +1,14 @@
+package org.wq.jvm.chapter02;
+
+public class ClinitTest {
+    private int a = 1;
+    private static int c = 2;
+    public ClinitTest() {
+        a = 10;
+        int d = 20;
+    }
+
+    public static void main(String[] args) {
+        int b = 2;
+    }
+}
