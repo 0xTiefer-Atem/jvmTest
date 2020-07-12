d34f707a97f6c06d2d2c29f436c2ec85a76c37a7
0xMilk <1144502582@qq.com>
2020-07-12 22:05:28 +0800

    clinit的生成

---
+++ src/main/java/org/wq/jvm/chapter02/ClinitTest1.java
@@ -0,0 +1,19 @@
+package org.wq.jvm.chapter02;
+
+public class ClinitTest1 {
+    static class Father {
+        public static int A = 1;
+        static {
+            A = 2;
+        }
+    }
+
+    static class Child extends Father{
+        public static int B = A;
+    }
+
+    public static void main(String[] args) {
+        //先加载Father类，在加载Child类
+        System.out.println(Child.B);
+    }
+}
