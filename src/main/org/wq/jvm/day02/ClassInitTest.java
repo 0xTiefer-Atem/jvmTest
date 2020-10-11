f892809702a624a3531b6d75c852cdb55183aa26
Wq <1144502582@qq.com>
2020-10-11 17:13:22 +0800

    格式化代码

--- src/main/org/wq/jvm/day02/ClassInitTest.java
+++ src/main/org/wq/jvm/day02/ClassInitTest.java
@@ -4,6 +4,7 @@ package org.wq.jvm.day02;
 //有static修饰时，jvm才会生成<clinit>()方法，没有就不会生成
 public class ClassInitTest {
     private static int num = 1;
+
     static {
         num = 2;
         number = 20;
