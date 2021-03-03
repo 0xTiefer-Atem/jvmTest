3ca8cd3830ff8096971d716642e5e39657b03474
Wq <1144502582@qq.com>
2021-03-03 09:26:05 +0800

    格式化代码

--- src/main/org/wq/jvm/day04/TestAllocation_01.java
+++ src/main/org/wq/jvm/day04/TestAllocation_01.java
@@ -13,9 +13,9 @@ public class TestAllocation_01 {
     private static final int _1MB = 1024 * 1024;
 
     public static void main(String[] args) {
-        byte[] allocation1 = new byte[2* _1MB];
-        byte[] allocation2 = new byte[2* _1MB];
-        byte[] allocation3 = new byte[2* _1MB];
-        byte[] allocation4 = new byte[4* _1MB];//出现一次 Minor GC
+        byte[] allocation1 = new byte[2 * _1MB];
+        byte[] allocation2 = new byte[2 * _1MB];
+        byte[] allocation3 = new byte[2 * _1MB];
+        byte[] allocation4 = new byte[4 * _1MB];//出现一次 Minor GC
     }
 }
